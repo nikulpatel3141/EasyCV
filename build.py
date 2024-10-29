c6f267ff05b8d57f22e230225d616f6c5ca7c7ed
Nikul Patel <nikulpatel3141@users.noreply.github.com>
2024-10-29 13:15:53 +0000

    iterate on build file

--- build.py
+++ build.py
@@ -9,17 +9,38 @@
 import datetime as dt
 from pathlib import Path
 import tomllib
+import logging
+from typing import Any
 
 import typst
 
+logging.basicConfig(level=logging.INFO)
 
+SOURCE_DIR = Path(__file__).parent / "src"
 BUILD_DIR = Path(__file__).parent / "build"
 
 
-def compile_file(source: Path) -> Path:
-    output_filename = source.stem + dt.date.today().strftime("_%b_%y") + ".pdf"
+def load_config() -> dict[str, Any]:
+    with open(SOURCE_DIR / "config.toml", "rb") as f:
+        config = tomllib.load(f)
+    return config
+
+
+def main() -> None:
+    config = load_config()
+    name = config["name"]
     
-    typst.compile(source, BUILD_DIR / output_filename)
+    prefix, suffix = name.replace(' ', '_'), dt.date.today().strftime('%b_%y') + ".pdf"
+
+    BUILD_DIR.mkdir(exist_ok=True)
+    for _ in BUILD_DIR.glob("*.pdf"):
+        _.unlink()
+
+    for source in SOURCE_DIR.glob("*.typ"):
+        output = BUILD_DIR / "_".join([prefix, source.stem, suffix])
+        logging.info(f"Compiling Typst file {source} -> {output}")
+        typst.compile(source, output)
 
 
-compile_file(Path(__file__).parent / "src" / "CV.typ")
+if __name__ == "__main__":
+    main()
